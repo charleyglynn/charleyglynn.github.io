21a25966eb8b9b5f4fe57a54d7a56183b07a30af
Charley Glynn <charleyglynn@virginmedia.com>
2018-03-18 22:16:44 +0000

    bath 2 and 3D

---
+++ Bathy-2/layers/layers.js
@@ -0,0 +1,41 @@
+var wms_layers = [];
+var lyr_GEBCO2014_400_346602_192233_740777_30Sec_Geotiffcopycopy_0 = new ol.layer.Image({
+                            opacity: 1,
+                            title: "GEBCO2014_-40.0_34.6602_19.2233_74.0777_30Sec_Geotiff copy copy",
+                            
+                            
+                            source: new ol.source.ImageStatic({
+                               url: "./layers/GEBCO2014_400_346602_192233_740777_30Sec_Geotiffcopycopy_0.png",
+    attributions: [new ol.Attribution({html: '<a href=""></a>'})],
+                                projection: 'EPSG:3857',
+                                alwaysInRange: true,
+                                imageExtent: [-4452779.631731, 4117799.027355, 2139927.967366, 12546999.748556]
+                            })
+                        });var lyr_GEBCO2014_400_346602_192233_740777_30Sec_Geotiff_1 = new ol.layer.Image({
+                            opacity: 1,
+                            title: "GEBCO2014_-40.0_34.6602_19.2233_74.0777_30Sec_Geotiff",
+                            
+                            
+                            source: new ol.source.ImageStatic({
+                               url: "./layers/GEBCO2014_400_346602_192233_740777_30Sec_Geotiff_1.png",
+    attributions: [new ol.Attribution({html: '<a href=""></a>'})],
+                                projection: 'EPSG:3857',
+                                alwaysInRange: true,
+                                imageExtent: [-4452779.631731, 4117799.027355, 2139927.967366, 12546999.748556]
+                            })
+                        });var lyr_GEBCO2014_400_346602_192233_740777_30Sec_Geotiffcopy_2 = new ol.layer.Image({
+                            opacity: 1,
+                            title: "GEBCO2014_-40.0_34.6602_19.2233_74.0777_30Sec_Geotiff copy",
+                            
+                            
+                            source: new ol.source.ImageStatic({
+                               url: "./layers/GEBCO2014_400_346602_192233_740777_30Sec_Geotiffcopy_2.png",
+    attributions: [new ol.Attribution({html: '<a href=""></a>'})],
+                                projection: 'EPSG:3857',
+                                alwaysInRange: true,
+                                imageExtent: [-4452779.631731, 4117799.027355, 2139927.967366, 12546999.748556]
+                            })
+                        });
+
+lyr_GEBCO2014_400_346602_192233_740777_30Sec_Geotiffcopycopy_0.setVisible(true);lyr_GEBCO2014_400_346602_192233_740777_30Sec_Geotiff_1.setVisible(true);lyr_GEBCO2014_400_346602_192233_740777_30Sec_Geotiffcopy_2.setVisible(true);
+var layersList = [lyr_GEBCO2014_400_346602_192233_740777_30Sec_Geotiffcopycopy_0,lyr_GEBCO2014_400_346602_192233_740777_30Sec_Geotiff_1,lyr_GEBCO2014_400_346602_192233_740777_30Sec_Geotiffcopy_2];
